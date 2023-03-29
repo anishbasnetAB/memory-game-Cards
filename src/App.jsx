4a11678ba12c5b242d156981d9f2756a64ea6af0
anishbasnet_ab <anishbasnet027@gmail.com>
2023-03-29 14:47:52 +0545

    working flipped

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import './App.css'
 import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  {"src": "/img/1Ace.png"},
-  {"src": "/img/1jack.png"},
-  {"src": "/img/1king.png"},
-  {"src": "/img/2spades.png"},
-  {"src": "/img/4diamond.png"},
-  {"src": "/img/10spades.png"},
+  {"src": "/img/1Ace.png", matched:false},
+  {"src": "/img/1jack.png", matched:false},
+  {"src": "/img/1king.png", matched:false},
+  {"src": "/img/2spades.png", matched:false},
+  {"src": "/img/4diamond.png", matched:false},
+  {"src": "/img/10spades.png", matched:false},
 
 ]
 
@@ -38,15 +38,28 @@ const App = () => {
   useEffect(()=>{
     if(choiceOne && choiceTwo){
       if(choiceOne.src==choiceTwo.src){
-        console.log('Matches')
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src){
+              return {
+                ...card, matched:true
+              }
+            }
+            else {
+              return card
+            }
+          })
+        })
         resetTurn()
       }
       else{
-        console.log("NOT match")
+        
         resetTurn()
       }
     }
   },[choiceOne,choiceTwo])
+
+  console.log(cards);
 
   //reset Choice and increase turn
   const resetTurn = () => {
@@ -62,7 +75,7 @@ const App = () => {
          <button  type="button" className="btn btn-outline-danger btn-lg mb-4" onClick={shuffleCards}>Start Game</button>
          <div className="card-grid ">
           {cards.map(card => (
-            <SingleCard key={card.id} card={card} handleChoice={handleChoice}/>
+            <SingleCard key={card.id} card={card} handleChoice={handleChoice} flipped={card === choiceOne || card === choiceTwo || card.matched}/>
           ))}
          </div>
       </div>
