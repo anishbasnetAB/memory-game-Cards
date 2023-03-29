2b6d4e1526402c03ad2d8fc5b95fc2c3cfce2241
anishbasnet_ab <anishbasnet027@gmail.com>
2023-03-29 20:38:23 +0545

    completed with well format

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,116 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
-import './App.css'
+import React, { useState, useEffect } from 'react';
 import SingleCard from './components/SingleCard';
+import './App.css';
 
-const cardImages = [  {"src": "/img/1Ace.png", matched:false},  {"src": "/img/1jack.png", matched:false},  {"src": "/img/1king.png", matched:false},  {"src": "/img/2spades.png", matched:false},  {"src": "/img/4diamond.png", matched:false},  {"src": "/img/10spades.png", matched:false},]
+const cardImages = [
+  { src: "/img/1Ace.png", matched: false },
+  { src: "/img/1jack.png", matched: false },
+  { src: "/img/1king.png", matched: false },
+  { src: "/img/2spades.png", matched: false },
+  { src: "/img/4diamond.png", matched: false },
+  { src: "/img/10spades.png", matched: false },
+];
 
 const App = () => {
-
-  const [cards , setCards] = useState([]);
+  // Game state
+  const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
-  const [disabled, setDisabled]= useState(false);
+  const [disabled, setDisabled] = useState(false);
   const [gameOver, setGameOver] = useState(false);
 
-  //shuffle cards
+  // Shuffle cards on start or restart of the game
   const shuffleCards = () => {
-    const shuffledCards = [...cardImages,...cardImages].sort(()=>Math.random()-0.5)
-    .map((card)=> ({...card, id:Math.random()}))
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
 
-    setCards(shuffledCards)
-    setTurns(0)
+    setCards(shuffledCards);
+    setTurns(0);
     setGameOver(false);
-  }
+  };
 
-  //handle Choice
-  const handleChoice = (card) =>{
-    choiceOne ? setChoiceTwo(card):setChoiceOne(card)
-  }
+  // Handle choice of a card
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
 
-  //compare 2 selected cards 
-  useEffect(()=>{
-    if(choiceOne && choiceTwo){
-      setDisabled(true)
-      if(choiceOne.src===choiceTwo.src){
-        setCards(prevCards => {
-          const newCards = prevCards.map(card => {
-            if (card.src === choiceOne.src){
-              return { ...card, matched:true}
-            }else {
-              return card
+  // Compare two selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true);
+      if (choiceOne.src === choiceTwo.src) {
+        // Update matched state for matched cards
+        setCards((prevCards) => {
+          const newCards = prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
             }
           });
-          if (newCards.every(card => card.matched)) {
+
+          // Check if all cards are matched
+          if (newCards.every((card) => card.matched)) {
             setGameOver(true);
           }
           return newCards;
         });
-        resetTurn()
-      }
-      else{
-        setTimeout(()=>resetTurn(),1000)
+        // Reset choice and increase turn
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000);
       }
     }
-  },[choiceOne,choiceTwo])
+  }, [choiceOne, choiceTwo]);
 
-  //reset Choice and increase turn
+  // Reset choice and increase turn
   const resetTurn = () => {
-    setChoiceOne(null)
-    setChoiceTwo(null)
-    setTurns(prevTruns => prevTruns+1)
-    setDisabled(false)
-  }
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTruns) => prevTruns + 1);
+    setDisabled(false);
+  };
 
-  //restart game
+  // Restart the game
   const handleRestart = () => {
     shuffleCards();
-  }
+  };
 
   return (
-    <div className='appBg '>
-      <div className='text-center m-2'>
-         <h1 className='p-2'>Memory Game</h1>
-         <button  type="button" className="btn btn-outline-danger btn-lg mb-4" onClick={shuffleCards}>Start Game</button>
-         <div className="card-grid ">
-          {cards.map(card => (
-            <SingleCard key={card.id} card={card} handleChoice={handleChoice} flipped={card === choiceOne || card === choiceTwo || card.matched} disabled = {disabled}/>
+    <div className="appBg">
+      <div className="text-center m-2">
+        <h1 className="p-2">Memory Game</h1>
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-lg mb-4"
+          onClick={shuffleCards}
+        >
+          Start Game
+        </button>
+        <div className="card-grid">
+          {cards.map((card) => (
+            <SingleCard
+              key={card.id}
+              card={card}
+              handleChoice={handleChoice}
+              flipped={
+                card === choiceOne || card === choiceTwo || card.matched
+              }
+              disabled={disabled}
+            />
           ))}
-         </div>
-         {gameOver && 
-          <div className="text-center mt-4">
-            <h2>Congratulations! You've matched all the cards in {turns} turns.</h2>
-            <button type="button" className="btn btn-outline-danger btn-lg mt-4" onClick={handleRestart}>Play Again</button>
-          </div>
-         }
       </div>
+        {gameOver && 
+        <div className="text-center mt-4">
+          <h2>Congratulations! You've matched all the cards in {turns} turns.</h2>
+          <button type="button" className="btn btn-outline-danger btn-lg mt-4" onClick={handleRestart}>Play Again</button>
+        </div>
+        }
       </div>
-    );
-    }
+    </div>
+  );
+}
   
-    export default App;
+export default App;
--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
-import './SingleCard.css'
+import React from "react";
+import "./SingleCard.css";
 
-const SingleCard = ({card, handleChoice, flipped,disabled}) => {
+const SingleCard = ({ card, handleChoice, flipped, disabled }) => {
 
-    const handleClick=()=>{
-        if(!disabled){
-            handleChoice(card)
-        }    
+  // Handle click event for a card
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
     }
+  };
 
   return (
-    <div className='card' >
-        <div className={flipped ? "flipped":""}>
-        <img className="front "   src={card.src} alt="card front side"/>
-        <img className='back'  src='/img/card-back.png' onClick={handleClick} alt="card back side"/>
-        </div>
+    <div className="card mt-4">
+      <div className={flipped ? "flipped" : ""}>
+        <img
+          className="front"
+          src={card.src}
+          alt="card front side"
+        />
+        <img
+          className="back"
+          src="/img/card-back.png"
+          onClick={handleClick}
+          alt="card back side"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SingleCard
+export default SingleCard;
